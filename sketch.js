d1fddd2f42d97ca2132607ada0191fc7a6d9222b
FabioRNobrega <36975012+FabioRNobrega@users.noreply.github.com>
2018-05-26 21:43:46 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,6 +11,5 @@ function draw()
   background(constrain(abs(mouseX-pmouseX) * 30, 30, 200));
 
   rect(width/2, height/2, abs(mouseX-pmouseX) * 5, abs(mouseX-pmouseX) * 5);
-
-  //println(abs(mouseX - pmouseX));
+  
 }
